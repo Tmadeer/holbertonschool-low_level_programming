61571bf8e212272b95819d5615a986a30da3c796
Tmadeer <imorganista_@hotmail.com>
2026-07-06 13:21:50 +0300

    Fix Betty style: add blank line after declarations

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -12,16 +12,13 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = (rand() % 1000000000) - 500000000;
-
+	n = rand() - RAND_MAX / 2;
 	printf("%d is ", n);
-
 	if (n > 0)
 		printf("positive\n");
 	else if (n == 0)
 		printf("zero\n");
 	else
 		printf("negative\n");
-
 	return (0);
 }
