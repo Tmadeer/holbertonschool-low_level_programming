ba51a33688a99421acc7e869e683a97bc76642c6
Tmadeer <imorganista_@hotmail.com>
2026-07-10 23:11:42 +0300

    Add 9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers 1 to 100, replacing multiples of 3, 5, or both
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	n = 1;
+	while (n <= 100)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+			printf("FizzBuzz");
+		else if (n % 3 == 0)
+			printf("Fizz");
+		else if (n % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", n);
+
+		if (n != 100)
+			printf(" ");
+
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
