4c9a72a3ff3bf9806dd8d2daa46e4c746b902a58
Tmadeer <imorganista_@hotmail.com>
2026-07-15 20:37:17 +0300

    Fix 100-atoi.c and update main.h

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -26,10 +26,12 @@ int _atoi(char *s)
 		{
 			while (s[i] >= '0' && s[i] <= '9')
 			{
-				result = (result * 10) + (s[i] - '0');
+				result = (result * 10) - (s[i] - '0');
 				i++;
 			}
-			return (result * sign);
+			if (sign == 1)
+				result *= -1;
+			return (result);
 		}
 		else
 			sign = 1;
