f8c987f9999ff65a2094aaa2225bf990f3571a15
Tmadeer <imorganista_@hotmail.com>
2026-07-15 14:03:25 +0300

    Add 8-print_array.c and update main.h

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: pointer to the first element of the array
+ * @n: the number of elements to print
+ *
+ * Return: void (nothing)
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (i != 0)
+			printf(", ");
+		printf("%d", a[i]);
+		i++;
+	}
+	printf("\n");
+}
