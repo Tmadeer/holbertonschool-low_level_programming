598fa0276de813fc1e5027184fef881576631f7a
Tmadeer <imorganista_@hotmail.com>
2026-07-25 16:20:08 +0300

    Add 5-free_dog.c and finalize dog.h

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees memory allocated for a dog_t struct
+ * @d: struct dog to free
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
+
