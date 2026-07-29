a23807ecb84ed14dc09b0e1a1f4734a7369d620e
Tmadeer <imorganista_@hotmail.com>
2026-07-29 12:15:16 +0300

    Add 0-print_name.c and header

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name using a function pointer
+ * @name: name of the person
+ * @f: pointer to a function that prints the name
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+	{
+		f(name);
+	}
+}
+
