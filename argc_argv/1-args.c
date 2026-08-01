64dcd30d878a10104b40f72995adaa055da2fd56
Tmadeer <imorganista_@hotmail.com>
2026-08-01 22:24:01 +0300

    Add task 1 args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Prints the number of arguments passed into it.
+ * @argc: The number of command line arguments.
+ * @argv: An array containing the program command line arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
+
