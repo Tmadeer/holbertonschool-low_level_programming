b0d134fb3abd6f01e72806f1c5be011ef127b96f
Tmadeer <imorganista_@hotmail.com>
2026-08-10 13:21:21 +0300

    Fix task 7: remove dependency on dlistint_len

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -12,34 +12,36 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new;
-	dlistint_t *prev_node;
-	unsigned int len, i;
+	dlistint_t *temp;
+	unsigned int i;
 
 	if (h == NULL)
 		return (NULL);
-
-	len = dlistint_len(*h);
-
-	if (idx > len)
-		return (NULL);
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	if (idx == len)
-		return (add_dnodeint_end(h, n));
 
-	prev_node = *h;
-	for (i = 0; i < idx - 1; i++)
-		prev_node = prev_node->next;
+	temp = *h;
+	i = 0;
+	while (temp != NULL && i < idx - 1)
+	{
+		temp = temp->next;
+		i++;
+	}
+
+	if (temp == NULL)
+		return (NULL);
+	if (temp->next == NULL)
+		return (add_dnodeint_end(h, n));
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 
 	new->n = n;
-	new->next = prev_node->next;
-	new->prev = prev_node;
-	prev_node->next->prev = new;
-	prev_node->next = new;
+	new->next = temp->next;
+	new->prev = temp;
+	temp->next->prev = new;
+	temp->next = new;
 
 	return (new);
 }
